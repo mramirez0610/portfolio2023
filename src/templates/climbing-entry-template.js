60da8ec458388245a4ef893740f0e575439e2f5b
marco <marcoramirezmail@gmail.com>
2025-10-03 15:02:31 -0700

    scroll

--- src/templates/climbing-entry-template.js
+++ src/templates/climbing-entry-template.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as styles from "@styles/pages/climbing/cEntry.module.scss";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import ClimbingLayout from "@components/climbing/climbingLayout";
 import Markdown from "react-markdown";
 
@@ -14,6 +15,10 @@ export default function Entry({ pageContext }) {
         <div className={styles.entry}>
           <div className={styles.frontmatter}></div>
           <h5>{frontmatter.date}</h5>
+          <GatsbyImage
+            image={getImage(frontmatter.image)}
+            alt={frontmatter.title}
+          />
           <Markdown>{body}</Markdown>
         </div>
       </ClimbingLayout>
