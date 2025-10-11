ec7edfa01a9e58f48277b1567e9cf46ebf024a4e
marco <marcoramirezmail@gmail.com>
2025-10-11 12:48:12 -0700

    climbing about page

--- src/pages/climbing/about.js
+++ src/pages/climbing/about.js
@@ -1,15 +1,32 @@
 import * as React from "react";
 import ClimbingLayout from "../../components/climbing/climbingLayout";
+import * as styles from "@styles/pages/climbing/cAbout.module.scss";
 import Seo from "@components/seo";
 
 export default function ClimbingAbout() {
   return (
     <ClimbingLayout>
-      <div>about</div>
+      <div className={styles.cAbout}>
+        <h1>about</h1>
+
+        <div className={styles.content}>
+          <p>
+            what ever happened to blogs?? yea yea maybe they're a bit silly in
+            the 2020's since social media is Aywhere And Everywhere, but cmon
+            dude. if everyone had a blog i think people would be a lot happier.
+          </p>
+          <p>
+            i love rock climbing. i really REALLY love rock climbing, so i
+            figured i'd make a place to write my thoughts over my progression in
+            the sport, and how i'm currently feeling about it. thanks for
+            stopping by!
+          </p>
+        </div>
+      </div>
     </ClimbingLayout>
   );
 }
 
-export const Head = ({ pageContext }) => (
+export const Head = () => (
   <Seo title="about" meta={[{ name: "robots", content: "noindex" }]} />
 );
