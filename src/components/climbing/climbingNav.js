8a0d6a16a7968a6ef8e67b26b193ae1bdd5adab1
marco <marcoramirezmail@gmail.com>
2026-01-02 18:28:35 -0500

    mobile nav

--- src/components/climbing/climbingNav.js
+++ src/components/climbing/climbingNav.js
@@ -1,9 +1,15 @@
 import * as React from "react";
+import { useState } from "react";
 import { Link } from "gatsby";
 import * as styles from "@styles/components/climbing/climbingNav.module.scss";
-import { ArrowFatLeftIcon } from "@phosphor-icons/react";
+import {
+  ArrowFatLeftIcon,
+  ArrowFatLinesLeftIcon,
+  DotsNineIcon,
+} from "@phosphor-icons/react";
 
 export default function ClimbingNav() {
+  const [open, setOpen] = useState(false);
   return (
     <section className={styles.cNav}>
       {/* <div className={styles.logo}></div> */}
@@ -30,9 +36,17 @@ export default function ClimbingNav() {
             justifyContent: "center",
           }}
         >
-          <Link className={styles.link} to="/">
+          {/* <Link className={styles.link} to="/">
             <ArrowFatLeftIcon size={24} />
+            <ArrowFatLineLeft size={32} />
             portfolio
+          </Link> */}
+          <Link className={styles.link} to="/">
+            <span className={styles.iconSwap}>
+              <ArrowFatLeftIcon className={styles.iconPrimary} size={24} />
+              <ArrowFatLinesLeftIcon className={styles.iconHover} size={24} />
+            </span>
+            <span>portfolio</span>
           </Link>
         </div>
 
@@ -42,6 +56,30 @@ export default function ClimbingNav() {
         <Link className={styles.link} to="/climbing/about">
           about
         </Link>
+      </nav>
+
+      <div
+        className={`${styles.mobileNavButton} ${open ? styles.navOpen : ""}`}
+        onClick={() => setOpen(!open)}
+      >
+        <DotsNineIcon size={32} weight="bold" />
+      </div>
+
+      <nav className={`${styles.mobileNav} ${open ? styles.open : ""}`}>
+        <div className={styles.navLinks}>
+          <Link className={styles.link} to="/">
+            <span className={styles.iconSwap}>
+              <ArrowFatLeftIcon className={styles.iconPrimary} size={24} />
+            </span>
+            <span>portfolio</span>
+          </Link>
+          <Link className={styles.link} to="/climbing">
+            entries
+          </Link>
+          <Link className={styles.link} to="/climbing/about">
+            about
+          </Link>
+        </div>
       </nav>
     </section>
   );
